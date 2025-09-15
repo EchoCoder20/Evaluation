0c1dda990ff11d184bedcbbebff927368496d149
EchoCoder20 <albusaidi1915@gmail.com>
2025-09-15 11:25:54 +0400

    Modify the task

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,10 +1,9 @@
 public class TeenNumberChecker {
     public static boolean loneTeen(int age1, int age2) {
-        boolean isAge1Teen = isTeen(age1);
-        boolean isAge2Teen = isTeen(age2);
-        return isAge1Teen ^ isAge2Teen;
+           return isTeen(age1) ^ isTeen(age2);
     }
     private static boolean isTeen(int age) {
         return age >= 13 && age <= 19;
     }
+
 }
