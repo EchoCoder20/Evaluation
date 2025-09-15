c04617aa5f43deac41449b41cea45ddfc81bfa8f
EchoCoder20 <albusaidi1915@gmail.com>
2025-09-15 14:15:32 +0400

    Solving Bunney Task

---
+++ src/BunnyEarsTwoCalculator.java
@@ -0,0 +1,13 @@
+//Recursion-1 > bunnyEars2
+public class BunnyEarsTwoCalculator {
+    public int countBunnyEars(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        }
+        if (bunnies % 2 != 0) {
+            return 2 + countBunnyEars(bunnies - 1);
+        } else {
+            return 3 + countBunnyEars(bunnies - 1);
+        }
+    }
+}
