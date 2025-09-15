803b77a949c96ac0cd9e827c8f8fa2ce907d6f28
EchoCoder20 <albusaidi1915@gmail.com>
2025-09-15 11:57:56 +0400

    Modify the code again

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String message="Hello World";
+        final String message="Hello World";
         System.out.println(message);
     }
 }
